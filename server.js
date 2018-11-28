fb30494dc87faa454d8c5b718d5098684a80f814
Glitch (bright-skateboard) <none>
2018-11-28 16:43:03 +0000

    🍃🏠 Checkpoint ./server.js:318686/1674

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 // Basic Configuration 
 const port = process.env.PORT || 3000;
-const randomnumber = require('randomnumber')
+const randomstring = require('randomstring')
 
 /** this project needs a db !! **/ 
 mongoose.connect(process.env.MONGOLAB_URI, {
@@ -47,30 +47,30 @@ app.get('/', function(req, res){
 app.post("/api/shorturl/new", (req, res) => {
   console.log(req.body.url)
   let original_url = req.body.url
-  URL.find({"original_url": original_url}, (err, data) => {
-    if(err) {
-      let short_url = process.env.PROJECT_URL + randomnumber.generate(5)
-      URL.create({"original_url": original_url, "short_url": })
-    }
-    else {
-      console.log()
-    }
-  })
-
-})
-
-
-app.get("/api/shorturl/:original_url", function (req, res) {
-  let original_url = req.params.original_url
   let options = {all: true}
   dns.lookup(original_url, options, (err, addresses) => {
     if (err) {
       res.json({"error":"invalid URL"})
     } 
     else {
-      res.json({"original_url": req.params.original_url, "short_url": ""});
+      URL.find({"original_url": original_url}, (err, data) => {
+        if(err) {
+          let short_url = process.env.PROJECT_URL + randomstring.generate(5)
+          URL.create({"original_url": original_url, "short_url": short_url})
+        }
+        else {
+          console.log('short_url already exists.')
+          console.log(data)
+        }
+      })
     }
   })
+})
+
+
+app.get("/api/shorturl/:original_url", function (req, res) {
+  let original_url = req.params.original_url
+  
 });
 
 
